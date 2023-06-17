093b514385d5d5659cf9c92c14d5e10a6d926449
Faizou <faizouaremou@gmail.com>
2023-06-17 20:47:48 +0200

    fix tip calculator

--- bin/index.ts
+++ bin/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 // import minimist from 'minimist';
+
 import ReadLine from 'readline';
 const rlInterface = ReadLine.createInterface({
   input: process.stdin,
@@ -10,16 +11,51 @@ let billAmount: number;
 let tipRate: number;
 let tip: number;
 let total: number
-rlInterface.question(`What is the bill amount?`, (answer) => {
-  billAmount = Number(parseFloat(answer).toFixed(2));
-  rlInterface.question(`What is the tip rate?"`, (answer) => {
-    tipRate = parseInt(answer);
-    tip = (billAmount * tipRate)/100;
-    total = billAmount + tip
-    console.log(`Tip: ${tip}`);
-    console.log(`Total: ${total}`);
-    rlInterface.close();
-  })
-})
+const billAmountQuestion = 'What is the bill amount?'
+const tipAmountQuestion = `What is the tip rate?"`
+const validNumberText="amount you enter is not a valid number"
 
+
+
+askForBillAmount()
+
+
+
+function askForBillAmount() {
+  rlInterface.question(billAmountQuestion, (answer) => {
+    billAmount = Number(parseFloat(answer).toFixed(2));
+    if (isValid(billAmount)) {
+      askForTipAmount()
+    } else {
+      console.log(validNumberText);
+      askForBillAmount()
+    }
+
+  })
+}
+
+function askForTipAmount() {
+  rlInterface.question(tipAmountQuestion, (answer) => {
+    tipRate = parseInt(answer);
+    if (isValid(tipRate)) {
+      tip = (billAmount * tipRate) / 100;
+      total = billAmount + tip
+      console.log(`Tip: ${tip}`);
+      console.log(`Total: ${total}`);
+      rlInterface.close();
+    } else {
+      console.log(validNumberText);
+      askForTipAmount()
+    }
+  })
+}
+
+
+
+
+
+
+function isValid(billAmount: number): boolean {
+  return !Number.isNaN(billAmount) && billAmount >= 0;
+}
 
--- dist/bin/index.js
+++ dist/bin/index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 "use strict";
+// import minimist from 'minimist';
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// import minimist from 'minimist';
 const readline_1 = __importDefault(require("readline"));
 const rlInterface = readline_1.default.createInterface({
     input: process.stdin,
@@ -14,14 +14,38 @@ let billAmount;
 let tipRate;
 let tip;
 let total;
-rlInterface.question(`What is the bill amount?`, (answer) => {
-    billAmount = Number(parseFloat(answer).toFixed(2));
-    rlInterface.question(`What is the tip rate?"`, (answer) => {
-        tipRate = parseInt(answer);
-        tip = (billAmount * tipRate) / 100;
-        total = billAmount + tip;
-        console.log(`Tip: ${tip}`);
-        console.log(`Total: ${total}`);
-        rlInterface.close();
+const billAmountQuestion = 'What is the bill amount?';
+const tipAmountQuestion = `What is the tip rate?"`;
+const validNumberText = "amount you enter is not a valid number";
+askForBillAmount();
+function askForBillAmount() {
+    rlInterface.question(billAmountQuestion, (answer) => {
+        billAmount = Number(parseFloat(answer).toFixed(2));
+        if (isValid(billAmount)) {
+            askForTipAmount();
+        }
+        else {
+            console.log(validNumberText);
+            askForBillAmount();
+        }
     });
-});
+}
+function askForTipAmount() {
+    rlInterface.question(tipAmountQuestion, (answer) => {
+        tipRate = parseInt(answer);
+        if (isValid(tipRate)) {
+            tip = (billAmount * tipRate) / 100;
+            total = billAmount + tip;
+            console.log(`Tip: ${tip}`);
+            console.log(`Total: ${total}`);
+            rlInterface.close();
+        }
+        else {
+            console.log(validNumberText);
+            askForTipAmount();
+        }
+    });
+}
+function isValid(billAmount) {
+    return !Number.isNaN(billAmount) && billAmount >= 0;
+}
